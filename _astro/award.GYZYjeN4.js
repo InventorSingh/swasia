9e9d8f8e3a0f99b8e36664578e7a0bd91384ae77
Gurprit Singh <Singh.Gurprit@outlook.com>
2025-09-21 02:25:56 +0000

    Updates

--- _astro/award.GYZYjeN4.js
+++ _astro/award.GYZYjeN4.js
@@ -1,4 +1,4 @@
-import{c as a}from"./createLucideIcon.js";/**
+import{c as a}from"./translations.CZMRbY0w.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
