112848d048403df94a62f28187cf1dbab67e1fe6
Gurprit Singh <Singh.Gurprit@outlook.com>
2025-09-21 02:27:02 +0000

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ export default defineConfig({
   site: 'https://inventorsingh.github.io',
   base: '/swasia',
   outDir: './dist',
-  build: {
-    assets: 'assets'
-  },
   output: 'static',
   i18n: {
     defaultLocale: 'hi',
@@ -20,17 +17,5 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react()
-  ],
-  vite: {
-    build: {
-      assetsInlineLimit: 0,
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name][extname]',
-          chunkFileNames: 'assets/[name].js',
-          entryFileNames: 'assets/[name].js'
-        }
-      }
-    }
-  }
+  ]
 });
